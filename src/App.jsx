8d12faed333e9ee9e034bf6932d273fe30e74707
agayushh <agayush088@gmail.com>
2025-02-27 03:11:57 +0530

    improved card of all items

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,17 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Cart from "./components/Cart";
+import { useRecoilState } from "recoil";
+import { items } from "./components/state/atom";
+import { useEffect } from "react";
+import productList from "./components/data";
 
 function App() {
+  const [product, setProduct] = useRecoilState(items);
+
+  useEffect(() => {
+    setProduct(productList);
+  }, []);
   return (
     <div>
       <BrowserRouter>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,30 +1,38 @@
-import React, { useEffect } from "react";
-import { useRecoilState } from "recoil";
+import React from "react";
+import { useRecoilValue } from "recoil";
 import { items } from "./state/atom";
-import productList from "./data";
 
 export default function Home() {
-  const [product, setProduct] = useRecoilState(items);
-
-  useEffect(() => {
-    setProduct(productList);
-  }, []);
+  const product = useRecoilValue(items);
 
   return (
-    <div className="flex flex-wrap justify-center gap-4 p-4">
-      {product.map((samaan) => (
-        <div
-          key={samaan.id}
-          className="flex flex-col items-center w-64 p-4 border rounded-lg shadow-md"
-        >
-          <img
-            src={samaan.img}
-            alt=""
-            className="w-full h-full p-3 object-cover rounded-md"
-          />
-          <p className="mt-2 text-lg font-semibold">{samaan.model}</p>
-        </div>
-      ))}
+    <div className="flex flex-col justify-center w-screen">
+      <input
+        type="text"
+        placeholder="Search for item"
+        className="w-[40%] p-3 bg-slate-100 m-4 border-2 border-black"
+      />
+      <div className="flex flex-wrap justify-center gap-4 p-4">
+        {product.map((samaan) => (
+          <div
+            key={samaan.id}
+            className="flex flex-col items-center w-64 p-4 border rounded-lg shadow-md"
+          >
+            <img
+              src={samaan.img}
+              alt=""
+              className="w-full h-full p-3 object-cover rounded-md"
+            />
+            <p className="mt-2 text-lg font-semibold">{samaan.model}</p>
+            <p className="mt-2 text-lg font-semibold">{samaan.brand}</p>
+            <p className="mt-2 text-lg ">{samaan.space}</p>
+            <p className="mt-2 text-lg font-semibold">₹{samaan.price}</p>
+            <button className="bg-blue-600 text-white p-2 rounded-lg hover:bg-blue-400 mt-2">
+              Add to cart
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
